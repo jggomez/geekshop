326dc84dc33b40d8c00a84420413e9834d8c9399
Juan Guillermo Gomez Torres <juan.gomez01@gmail.com>
2019-09-10 01:39:26 -0500

    se crea pantallas de login

---
+++ app/src/main/java/co/devhack/tiendageek/presentation/SignInActivity.kt
@@ -0,0 +1,13 @@
+package co.devhack.tiendageek.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.devhack.tiendageek.R
+
+class SignInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
---
+++ app/src/main/java/co/devhack/tiendageek/presentation/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package co.devhack.tiendageek.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import co.devhack.tiendageek.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
